f6eed0ced11c2825ae30f1a2b4af690f4485f60c
Benjiman Walsh <walsh.ben@hotmail.com>
2025-04-03 04:47:15 +0000

    Solved challenge 4, but messy

--- Block Harbor/Proving Grounds Challenges/challenge4.sh
+++ Block Harbor/Proving Grounds Challenges/challenge4.sh
@@ -1,24 +1,20 @@
-cleanup() {
-    kill $isotprecv_pid
-    exit 0
-}
+NBYTES=255
 
 
 monitor_isotp() {
-   isotprecv -s 7E0 -d 7E8 -l vcan0 | cut -d ' ' -f 2-
-   # isotprecv -s 7E0 -d 7E8 -l vcan0 | while read a; do echo $a | cut -d ' ' -f 2-; done
+   while raw=$(timeout 2 isotprecv -s 7E0 -d 7E8 vcan0); do
+      printf "%s " "$(echo $raw  | xxd -r -s 1 -c $NBYTES)"
+   done
+
 }
 
-trap cleanup SIGINT SIGTERM
+ 
 
-monitor_isotp &
-isotprecv_pid=$!
+monitor_isotp & 
 
-
-for i in $(seq 0 8 255)
+for i in $(seq 0 1 256)
 do
-
-echo "23 14 C3 F8 00 $(printf "%02X" $i) 04" | isotpsend -s 7E0 -d 7E8 -g 1000000 vcan0
+   echo "23 14 C3 F8 $(printf "%02X 00 %02X" $i $NBYTES)" | isotpsend -s 7E0 -d 7E8 -g 500000 vcan0
 
 done
 
